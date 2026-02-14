674113265f3be4269e41828964057ca49e5b28ae
Santiago Solarte <santiagosolarte@unicauca.edu.co>
2026-02-14 15:59:12 -0500

    First commit

---
+++ medical-appointment-scheduling/src/main/java/co/unicauca/medical/appointment/scheduling/main/Main.java
@@ -0,0 +1,15 @@
+package co.unicauca.medical.appointment.scheduling.main;
+
+/**
+ * @author Valentina Añasco 
+ * @author Camila Dorado
+ * @author Felipe Gutierrez
+ * @author Ginner Ortega
+ * @author Santiago Solarte 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
